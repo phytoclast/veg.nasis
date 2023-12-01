c00bb41576162d9b1febf632190b0de7320b905b
phytoclast <greg.schmidt@mi.usda.gov>
2023-12-01 13:14:23 -0500

    Update processPlantTaxonomy.R

--- processPlantTaxonomy.R
+++ processPlantTaxonomy.R
@@ -4,7 +4,7 @@ library(sf)
 library(terra)
 library(soilDB)
 library(aqp)
-
+library(stringi)
 plants <- read.csv('D:/scripts/veg.nasis/data/plants/USDA Plants Database20231129.txt')
 saveRDS(plants, 'D:/scripts/veg.nasis/data/plants/plants20231129.RDS')
 
@@ -15,50 +15,71 @@ geo.select <- subset(geo, continent %in% 'NORTHERN AMERICA' | area %in% c('Puert
 kew.select <- subset(kew, plant_name_id %in% geo.select$plant_name_id)
 kew.select2 <- subset(kew, accepted_plant_name_id %in% kew.select$plant_name_id)
 
-m.ac <- read.csv('D:/scripts/vegnasis/data_raw/m.ac.csv')
-
-plants <- read.csv('D:/scripts/veg.nasis/data/plants/USDA Plants Database20231129.txt')
-extractTaxon <- function(rawnames, report = 'taxon'){
-x = data.frame(rawname = rawnames)
-x <- x |> mutate(rawname = str_replace_all(rawname,'^x\\s|^X\\s','×'),
-                           rawname = str_replace_all(rawname,'\\sx\\s|\\sX\\s',' ×'),
-                           genus = trimws(str_split_fixed(rawname,'[[:space:]]',2)[,1]),
-                           first = trimws(str_split_fixed(rawname,'[[:space:]]',2)[,2]),
-                           second = ifelse(grepl('^[a-z]|^×',first) & !grepl('^ex\\w',first),
-                                           trimws(str_split_fixed(first,'[[:space:]]',2)[,1]),""),
-                           xxx = ifelse(grepl('^[a-z]|^×',first) & !grepl('^ex\\w',first),
-                                        trimws(str_split_fixed(first,'[[:space:]]',2)[,2]),first),
-                           xxx = str_replace_all(xxx, "\\sssp.\\s|^ssp.\\s|\\ssubsp. |^subsp.\\s", '_ssp._' ),
-                           xxx = str_replace_all(xxx, "\\svar.\\s|^var.\\s", '_var._' ),
-                           xxx = ifelse(grepl('\\sf.\\s[a-z]',xxx)|grepl('^f.\\s[a-z]',xxx) & !grepl('_f._ex\\s',xxx), str_replace_all(xxx, "\\sf.\\s|^f.\\s", '_f._' ),xxx),                         xxx1 = trimws(str_split_fixed(xxx,'_',3)[,1]),
-                           xxx2 = trimws(str_split_fixed(xxx,'_',3)[,2]),
-                           xxx3 = trimws(str_split_fixed(xxx,'_',4)[,3]),
-                           xxx4 = trimws(str_split_fixed(xxx,'_',5)[,4]),
-                           xxx5 = trimws(str_split_fixed(xxx,'_',6)[,5]),
-                           epithet = second,
-                           binomialauthor = xxx1,
-                           infrarank1 = xxx2,
-                           infraname1 = trimws(str_split_fixed(xxx3,'[[:space:]]',2)[,1]),
-                           infraauthor1 = trimws(str_split_fixed(xxx3,'[[:space:]]',2)[,2]),
-                           infrarank2 = xxx4,
-                           infraname2 = trimws(str_split_fixed(xxx5,'[[:space:]]',2)[,1]),
-                           infraauthor2 = trimws(str_split_fixed(xxx5,'[[:space:]]',2)[,2]),
-                           # first=NULL,
-                           # second=NULL,
-                           # xxx=NULL,
-                           # xxx1=NULL,
-                           # xxx2=NULL,
-                           # xxx3=NULL,
-                           # xxx4=NULL,
-                           # xxx5=NULL,
-                           binomial = trimws(paste(genus, epithet)),
-                           taxon = trimws(paste(binomial, infrarank1, infraname1, infrarank2, infraname2)),
-                           author = trimws(ifelse(!infraauthor2 %in% "", infraauthor2, ifelse(!infraauthor1 %in% "", infraauthor1, binomialauthor)))
-)
-if(report == 'author'){return(x$author)}else if(report=='binomial'){return(x$binomial)}else if(report == 'genus'){return(x$genus)}else{return(x$taxon)}
-}
 
 
+plants <- plants |> mutate(taxon = extractTaxon(Scientific.Name.with.Author, 'taxon'),
+                           author = extractTaxon(Scientific.Name.with.Author, 'author'))
+plants <- plants |> mutate(auct = ifelse(grepl('auct',author) |
+                                           grepl('illeg.',author) |
+                                           grepl(' non',author), T,F))
 
-rawnames = c('Arnica angustifolia Vahl subsp. tomentosa (Macoun) G.W. Douglas & G. Ruyle-Douglas', 'Abies ×shastensis (Lemmon) Lemmon', 'Abies X shastensis (Lemmon) Lemmon','Abies balsamea ssp. lasiocarpa', 'Abies balsamea (L.) Mill. var. fallax (Engelm.) B. Boivin','Agastache pallidiflora (A. Heller) Rydb. ssp. pallidiflora var. greenei (Briq.) R.W. Sanders')
-extractTaxon(rawnames,'taxon')
+cleanEncoding <- function(x){
+  n = length(x)
+  for(i in 1:n){#i=1
+    enc = stri_enc_detect(x[i])
+    enc <- enc[[1]]$Encoding[1]
+    if(!enc %in% 'UTF-8'){
+      if(enc %in% 'windows-1252'){
+        x[i] <- stri_conv(x[i], from = 'windows-1252', to='UTF-8')
+      }else if(enc %in% 'ISO-8859-1'){
+        x[i] <- stri_conv(x[i], from = 'ISO-8859-1', to='UTF-8')
+      }else{
+        x[i] <- stri_conv(x[i], from = 'latin1', to='UTF-8')
+      }
+    }
+  }  
+  return(x)}
+
+
+whatEncoding <- function(x){
+  n = length(x)
+  for(i in 1:n){#i=1
+    enc <- stri_enc_detect(x[i])
+    enc <- enc[[1]]$Encoding[1]
+    x[i] <- enc}
+  return(x)}
+
+m.ac <- read.csv('D:/scripts/vegnasis/data_raw/m.ac.csv', encoding = 'UTF-8')
+#m.ac <- m.ac |> mutate(en1 = whatEncoding(syn), en2 = whatEncoding(syn.auth))
+
+m.ac <- m.ac |> mutate(acc = cleanEncoding(acc),
+                       acc.auth = cleanEncoding(acc.auth),
+                       syn = cleanEncoding(syn), 
+                       syn.auth = cleanEncoding(syn.auth))
+write.csv(m.ac, 'D:/scripts/vegnasis/data_raw/m.ac.csv', row.names = F)
+
+stri_conv(this, from = enc, to='utf-8')
+
+
+
+x = m.ac.latin1[57957,'syn.auth']
+stri_conv(x, from = 'windows-1254', to='UTF-8')
+stri_conv(x, from = 'windows-1250', to='UTF-8')
+
+enc <- stri_enc_detect(m.ac.latin1[57957,'syn.auth'])
+enc <- enc[[1]]$Encoding[1]
+enc = t(as.data.frame(enc))
+enc[[1]]$Encoding[1]
+
+m.ac.utf8 <- read.csv('D:/scripts/vegnasis/data_raw/m.ac.csv', encoding = 'UTF-8')
+m.ac.latin1 <- read.csv('D:/scripts/vegnasis/data_raw/m.ac.csv', encoding = 'latin1')
+m.ac <- m.ac |> mutate(acc = extractTaxon(acc), syn = extractTaxon(syn))
+
+bonap <- unique(data.frame(bonap = m.ac$syn, bonap.auth = m.ac$syn.auth))
+bonap <- bonap
+
+syn <- unique(data.frame(taxon = kew.select2$taxon_name, author=kew.select2$taxon_authors))
+
+syn <- syn |> mutate(taxon = extractTaxon(taxon))
+
+syn 
