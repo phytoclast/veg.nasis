ce54b8a7f4fd4196916131e7ffa2d49e8d6be2e9
phytoclast <greg.schmidt@mi.usda.gov>
2023-12-04 17:03:06 -0500

    Update wetloamy.R

--- wetloamy.R
+++ wetloamy.R
@@ -11,6 +11,7 @@ veg.site <- subset(veg.site,Latitude != 0 & Observer_Code %in% c('BEL.JH', 'TOL.
                      Year >=2011 & !Observation_Type %in% c('Bogus', 'Floristics')) #
 veg.site$Observation_Label <- veg.site$Observation_ID
 
+
 for(i in 1:ncol(veg.site)){
 if(!FALSE %in% grepl('Yes|No',veg.site[,i])){
   veg.site[,i] <- ifelse(veg.site[,i] %in% "Yes",1,0)
@@ -23,12 +24,18 @@ wetloamy <- subset(veg.site, Loamy %in% 1 & Hydric %in% 1 & Euic %in% 1 & Floodp
 
 veg <- clean.veg.log(veg.site, veg.spp)
 veg <- subset(veg, cover > 0)
+                       
+
 hascover <- veg |> group_by(plot) |> summarize(cover = cover.agg(cover)) |> subset(cover >= 10)
 veg <- subset(veg, plot %in% hascover$plot)
-veg.wetloamy <- subset(veg, plot %in% wetloamy$Observation_ID) |> fill.hts.df() |> fill.type.df() |> fill.nativity.df()
+veg <- subset(veg, plot %in% wetloamy$Observation_ID) |> fill.hts.df() |> fill.type.df() |> fill.nativity.df() |>
+  mutate(taxon = harmonize.taxa(taxon, fix=T, sensu = 'bonap'),
+         taxon = extractTaxon(taxon, 'binomial'), #use for analysis
+         usda = harmonize.taxa(taxon, fix=T, sensu = 'usda'),
+         symbol = fill.usda.symbols(usda)) #for entry into EDIT
 
 
-veg.abiotic <- subset(veg.site, Observation_ID %in% veg.wetloamy$plot,
+veg.abiotic <- subset(veg.site, Observation_ID %in% veg$plot,
                       select = c('Observation_ID', "Upper","Middle","Lower","Coastal",
                                            "Floodplain","Inland","Hydric","Nonhydric",
                                            "Aquatic","Wet","Moist","Dry",
@@ -45,7 +52,7 @@ veg.abiotic<- veg.abiotic[,-1]
 d.abiotic <- vegan::vegdist(veg.abiotic, method='euclidean')
 d.abiotic <- d.abiotic/max(d.abiotic)
 
-m <- make.plot.matrix(veg.wetloamy, tr = 'log', label='plot', taxon='taxon')
+m <- make.plot.matrix(veg, tr = 'log', label='plot', taxon='taxon')
 d = vegan::vegdist(m, method='bray')
 dtab1 <- as.data.frame(as.matrix(d))
 dtab2 <- as.data.frame(as.matrix(d.abiotic))
@@ -68,58 +75,49 @@ rdfgroup <- cbind(rdf,groupdf)
 rownames(rdfgroup) = NULL
 veg.group <- veg |> left_join(rdfgroup, by=join_by(plot == Observation_ID)) |> subset(!is.na(groups))
 
-rdfgrouped <- rdfgroup |> left_join(wetloamy, by=join_by(Observation_ID == Observation_ID, Community_Name == Community_Name, Site_Type == Site_Type)) |> subset(select = c("Observation_ID", "groups", "Community_Name", "Upper","Middle","Lower","Coastal",
-                                                                                                                                                                         "Floodplain","Inland","Hydric","Nonhydric",
-                                                                                                                                                                         "Aquatic","Wet","Moist","Dry",
-                                                                                                                                                                         "Mucky","Rocky","Sandy","Loamy",
-                                                                                                                                                                         "Calcareous", "Euic", "Dysic", "Salty",
-                                                                                                                                                                         "Fresh", "Natural", "Seminatural", "Cultural",
-                                                                                                                                                                         "Cold", "Cool", "Mild",
-                                                                                                                                                                         "Warm", "Hot", "Humid", "Subhumid",
-                                                                                                                                                                         "Arid", "Microthermal", "Mesothermal", "Megathermal","Forest"))
+rdfgrouped <- rdfgroup |> left_join(wetloamy, by=join_by(Observation_ID == Observation_ID, Community_Name == Community_Name, Site_Type == Site_Type)) |> subset(select = c("Observation_ID", "groups", "Community_Name", "Upper","Middle","Lower","Coastal",                                                                     "Floodplain","Inland","Hydric","Nonhydric",
+                                                                                                                                                                           "Aquatic","Wet","Moist","Dry",
+                                                                                                                                                                           "Mucky","Rocky","Sandy","Loamy",                                                                                                             "Calcareous", "Euic", "Dysic", "Salty",                                                                                                                    "Fresh", "Natural", "Seminatural", "Cultural",                                                                                                              "Cold", "Cool", "Mild",
+                                                                                                                                                                           "Warm", "Hot", "Humid", "Subhumid",
+                                                                                                                                                                           "Arid", "Microthermal", "Mesothermal", "Megathermal","Forest"))
 rdfgrouped <- rdfgrouped |> group_by(groups) |> summarise(across(c(Upper,Middle,Lower,Coastal,
-                                                Floodplain,Inland,Hydric,Nonhydric,
-                                                Aquatic,Wet,Moist,Dry,
-                                                Mucky,Rocky,Sandy,Loamy,
-                                                Calcareous, Euic, Dysic, Salty,
-                                                Fresh, Natural, Seminatural, Cultural,
-                                                Cold, Cool, Mild,
-                                                Warm, Hot, Humid, Subhumid,
-                                                Arid, Microthermal, Mesothermal, Megathermal,Forest), mean))
+                                                                   Floodplain,Inland,Hydric,Nonhydric,
+                                                                   Aquatic,Wet,Moist,Dry,
+                                                                   Mucky,Rocky,Sandy,Loamy,
+                                                                   Calcareous, Euic, Dysic, Salty,
+                                                                   Fresh, Natural, Seminatural, Cultural,
+                                                                   Cold, Cool, Mild,
+                                                                   Warm, Hot, Humid, Subhumid,
+                                                                   Arid, Microthermal, Mesothermal, Megathermal,Forest), mean))
 
 
 #grouped summary
 #
-veg.summary <- veg.group |> fill.nativity.df() |> fill.type.df() |> fill.hts.df() |> mutate(symbol = fill.usda.symbols(taxon))
+veg.summary <- veg.group
 
 veg.summary2  <- veg.summary |>  summary.ESIS(group='groups', lowerQ = 0.5, upperQ = 0.95, normalize = TRUE, breaks = c(0.5, 2, 5, 15))
 
+write.csv(veg.summary2, 'wetloamy.csv', row.names = F)
 
-coverbounds <- function(cover.Low, cover.High, cover.min, cover.max, cover.mean){
-  upper.x = cover.High + (cover.mean - (cover.High+cover.Low)/2)
-  lower.x = cover.Low + (cover.mean - (cover.High+cover.Low)/2)
-  upper.x = upper.x + (lower.x - pmax(lower.x, cover.min))
-  lower.x = pmax(lower.x, cover.min)
-  lower.x = lower.x + (upper.x - pmin(upper.x, cover.max))
-  upper.x = pmin(upper.x, cover.max)
-  return(c(lower.x,upper.x))}
+#need function to convert to plant type categories and Nativity used by EDIT
 
-#example numbers
-cover.min = 10
-cover.Low = 27.5
-cover.mean = 33.8
-cover.High = 74.0
-cover.max = 80.0
-x = coverbounds(cover.Low, cover.High, cover.min, cover.max, cover.mean)
-x
-mean(x)
 
-#species with frq of 0.444
-cover.min = 0
-cover.50 = 0.0
-cover.mean = 9
-cover.95 = 10
-cover.max = 12
-x = coverbounds(cover.50, cover.95, cover.min, cover.max, cover.mean)
-x
-mean(x)
+group_community <- data.frame(plot = veg.summary2$group,
+                              taxon = veg.summary2$taxon,
+                              symbol = veg.summary2$symbol,
+                              type = veg.summary2$type,
+                              nativity=veg.summary2$nativity,
+                              cover=veg.summary2$cover.mean,
+                              stratum.min = veg.summary2$stratum.min,
+                              stratum.max = veg.summary2$stratum.max,
+                              ht.min = veg.summary2$Bottom,
+                              ht.max = veg.summary2$Top) |> pre.fill.veg()
+
+
+group_community.ass <- group_community |> fill.hts.df()
+
+  
+  
+group_community.ass <- get.assoc(group_community.ass)
+
+
