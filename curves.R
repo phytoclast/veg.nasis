e767b9d62750c275b6bada1ea8b8e3b76713e21e
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2025-03-16 22:43:33 -0400

    Update curves.R

--- curves.R
+++ curves.R
@@ -6,8 +6,8 @@ df <- data.frame(
   y=c(rnorm(50,5,5),20,20),
   s = NA)
 
-ggplot(df, aes(x=x,y=y))+
-  geom_point()
+# ggplot(df, aes(x=x,y=y))+
+#   geom_point()
 
 df2 <- cavhull(df$x,df$y, concave=F)
 ch <- chull(df$x,df$y)
@@ -16,12 +16,13 @@ df$s <- NA
 df[ch,]$s <- chn
 df2 <- subset(df, !is.na(s)) |> arrange(s)
 
-ggplot()+
-  geom_point(data=df, aes(x=x,y=y))+
-  geom_path(data=df2, aes(x=x,y=y))
+# ggplot()+
+#   geom_point(data=df, aes(x=x,y=y))+
+#   geom_path(data=df2, aes(x=x,y=y))
 
 dp <- 50-0
 
+concave=TRUE
 
 sc=1
 df$s <- NA
@@ -36,20 +37,18 @@ for(i in 1:(50*sc)){# (10*sc) i=1
 
 df3 <- subset(df, !is.na(s)) |> arrange(s)
 
-ggplot()+
-  geom_point(data=df, aes(x=x,y=y))+
-  geom_path(data=df3, aes(x=x,y=y))
-  coord_fixed()
+# ggplot()+
+#   geom_point(data=df, aes(x=x,y=y))+
+#   geom_path(data=df3, aes(x=x,y=y))
+#   coord_fixed()
   x=df$x
   y=df$y
   # cavhull2 <- function(x,y, concave = TRUE){
-    n=10 #number of segments to search between convex faces
+    n=5 #number of segments to search between convex faces
     df <- data.frame(x=floor(x*1000)/1000,y=floor(y*1000)/1000) |> unique()
     df <- df |> mutate(q = 1:nrow(df))
 
     #convex hull ----
-    wave0 <- data.frame(x=(0:(n+1))/(n+1))
-    wave0 <- wave0 |> mutate(a=x*2*pi,y=(cos(a)-1)/2)
     check <- TRUE #stopping rule
     #find starting point at bottom of plot
     miny <- min(df$y)
@@ -91,13 +90,14 @@ ggplot()+
       }
     }
     #concave hull first degree ----
-    df <- df |> mutate(s1 = s)
+    df <- df |> mutate(s1 = s, type = 'core')
+    
 
     if(concave){
       smax <- max(df$s, na.rm = TRUE)
       #visit each convex hull boundary and rotate to a common reference
-      for(i in 1:smax){#i=1
-        i0=ifelse(i == 1,smax,i-1)
+      for(i in 1:smax){#i=2
+        i0 = ifelse(i == 1,smax,i-1)
         x0 <- df[df$s %in% (i0),]$x
         y0 <- df[df$s %in% (i0),]$y
         x1 <- df[df$s %in% i,]$x
@@ -106,48 +106,56 @@ ggplot()+
         a0 = acos((x1-x0)/l0)
         a0 = ifelse(y1 - y0 >=0,a0,-1*a0)
         dfr <- vegnasis::rotate(x=df$x, y=df$y, a=a0/2/pi*360, cx=x0, cy=y0)
-        df <- df |> mutate(xr= x-x0,
-                           yr= y-y0,
-                           h=(xr^2+yr^2)^0.5,
-                           a1=acos(yr/h),
-                           a1=ifelse(xr >=0,a1,-1*a1),
-                           a1= a1+a0,
-                           xr = ifelse(h==0,0,h*sin(a1)),
-                           yr = ifelse(h==0,0,h*cos(a1)))
-        df <- df |> mutate(xr2= dfr$x-x0,
-                           yr2= dfr$y-y0)
+        df <- df |> mutate(xr= dfr$x-x0,
+                           yr= dfr$y-y0)
         #adjust wave
-        en <- pmax(3,pmin(n,l0*n/1))
-        wave0 <- data.frame(x=(0:(en+1))/(en+1))
-        wave0 <- wave0 |> mutate(a=x*2*pi,y=(cos(a)-1)/2)
-        wave <- data.frame(x=NA, y=NA, q=NA,s=NA,l1=NA,a1=NA,xr=wave0$x*l0,
-                           yr=wave0$y*l0,h=NA,s1=NA)
-        wavr <- vegnasis::rotate(x=wave$xr, y=wave$yr, a=-a0/2/pi*360, cx=0,cy=0)
-        wave <- wave |> mutate(x=wavr$x+x0,y=wavr$y+y0)
-
+        en <- pmax(3,floor(pmin(n,l0/5)))*3
+        #get max wave depth
+        psmax01 <- max(subset(df, xr >  (1-1)/3*l0 & xr < 1/n*l0)$yr)
+        psmax02 <- max(subset(df, xr >= (2-1)/3*l0 & xr < 2/n*l0)$yr)
+        psmax03 <- max(subset(df, xr >= (3-1)/3*l0 & xr < 3/n*l0)$yr)
+        psmax01 <- ifelse(is.infinite(psmax01),NA,psmax01)
+        psmax02 <- ifelse(is.infinite(psmax02),NA,psmax02)
+        psmax03 <- ifelse(is.infinite(psmax03),NA,psmax03)
         
-        ggplot()+
-          geom_point(data=df,aes(x=xr,y=yr))+
-          geom_point(data=df,aes(x=xr2,y=yr2), color='red')+
-          coord_fixed()
-
-
+        psmean <- weighted.mean(c(psmax01,psmax02,psmax03),
+                                w=c(2,5,2), na.rm =TRUE)
+        newdepth <- ifelse(is.na(abs(psmean)),l0,pmin(l0,abs(psmean)))
+        wave0 <- data.frame(x=(0:(en+1))/(en+1))
+        wave0 <- wave0 |> mutate(a=x*2*pi,y=(cos(a)^1-1)/2)
+        wave <- data.frame(x=NA, y=NA, q=NA,s=NA,l1=NA,a1=NA,xr=wave0$x*l0,
+                           yr=wave0$y*newdepth,h=NA,s1=NA, type='wave')
+        wavr <- vegnasis::rotate(x=wave$xr, y=wave$yr, a=-a0/2/pi*360, cx=0,cy=0)
+        wave <- wave |> mutate(x=wavr$x+x0,y=wavr$y+y0) |> subset(!yr >=0)
+        df <- df |> rbind(rbind(wave))
+       
 
         #find nearest vertex within n segments of the hull boundary
-        for(j in 1:n){#j=1
-          psmin1 <- (subset(df, xr >= (j-1)/n & xr < j/n)$yr)
+        for(j in 1:n){#j=3
+          psmax1 <- (subset(df, xr >= (j-1)/n*l0 & xr < j/n*l0)$yr)
 
-          smin1 <- ifelse(length(psmin1)>0,min(psmin1),1)
+          smax1 <- max(psmax1)
 
-          df <- df |> mutate(s1 = case_when(xr >= (j-1)/n & xr < j/n & yr %in% smin1 & yr < 0.5 ~ i0+j/n/100,
+          df <- df |> mutate(s1 = case_when(xr >= (j-1)/n*l0 & xr < j/n*l0 &
+                                              yr %in% smax1 ~ i0+j/n/100,
                                             TRUE ~ s1))
         }
-      }}
+      }
+      df <- df |> subset(type %in% 'core' | !is.na(s1))
+      }
 
 
+    df2 <- subset(df, !is.na(s)) |> arrange(s)
+    df3 <- subset(df, !is.na(s1)) |> arrange(s1)
+    df4 <- subset(df3, type %in% 'wave')
+    
 
-
-
+ggplot()+
+  geom_point(data=df,aes(x=x,y=y))+
+  geom_path(data=df3,aes(x=x,y=y), color='blue')+
+  geom_path(data=df2,aes(x=x,y=y), color='red')+
+  geom_point(data=df4,aes(x=x,y=y), color='green')+
+  coord_fixed()
 
 
 
