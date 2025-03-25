ef56e4a5b272624319e561ebd6bd5d9abac9b40e
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2025-03-24 22:20:04 -0400

    Update curves.R

--- curves.R
+++ curves.R
@@ -7,8 +7,8 @@ renumber <- function(x){
   dfsort <- df[order(df$s),]
   n2 <- length(x[!is.na(x)])
   if(n2>0){
-  dfsort[!is.na(dfsort$s),]$neworder <- 1:n2
-  s <- dfsort[order(dfsort$ind),]$neworder}else{s <- x}
+    dfsort[!is.na(dfsort$s),]$neworder <- 1:n2
+    s <- dfsort[order(dfsort$ind),]$neworder}else{s <- x}
   return(s)}
 
 
@@ -131,24 +131,24 @@ cavhull2 <- function(x,y, concavity = 0, curvy = FALSE, mag = 1, deep=FALSE){
         df$microinc <- NA
         #introduce wave
         if(curvy & k == concavity){
-        wave0 <- data.frame(x=(0:(en+1))/(en+1))
-        wave0 <- wave0 |> mutate(a=x*2*pi,y=(cos(a)^1-1)/2*mag)
-        wave <- data.frame(x=NA, y=NA, s=NA,l1=NA,a1=NA,xr=wave0$x*l0,
-        yr=wave0$y*l0*currat*currat2,h=NA,s1=NA, type='wave',
-        xs=NA,xa=NA,ys=NA,yl0=NA,ydiff=NA,microinc=NA)
-        wavr <- vegnasis::rotate(x=wave$xr, y=wave$yr, a=-a0/2/pi*360, cx=0,cy=0)
-        wave <- wave |> mutate(x=wavr$x+x0,y=wavr$y+y0) |> subset(!yr >=0)
-        df <- df |> rbind(rbind(wave))
+          wave0 <- data.frame(x=(0:(en+1))/(en+1))
+          wave0 <- wave0 |> mutate(a=x*2*pi,y=(cos(a)^1-1)/2*mag)
+          wave <- data.frame(x=NA, y=NA, s=NA,l1=NA,a1=NA,xr=wave0$x*l0,
+                             yr=wave0$y*l0*currat*currat2,h=NA,s1=NA, type='wave',
+                             xs=NA,xa=NA,ys=NA,yl0=NA,ydiff=NA,microinc=NA)
+          wavr <- vegnasis::rotate(x=wave$xr, y=wave$yr, a=-a0/2/pi*360, cx=0,cy=0)
+          wave <- wave |> mutate(x=wavr$x+x0,y=wavr$y+y0) |> subset(!yr >=0)
+          df <- df |> rbind(rbind(wave))
         }
         pickthispoint <- min(abs(subset(df, xs >= 0 & xs <=1)$ydiff))
         df <- df |> mutate(microinc = ifelse((xs >  0 & xs < 1  & round(abs(ydiff),10) %in% round(pickthispoint,10) | type %in% 'wave') & is.na(s1),xr,NA))
-       
+        
         df$microinc <- renumber(df$microinc)
         
         df <- df |> mutate(s1 = ifelse(!is.na(microinc) & is.na(s), i0+microinc/1000,s1))
-       df <- df |> subset(type %in% 'core' | !is.na(s1)) 
+        df <- df |> subset(type %in% 'core' | !is.na(s1)) 
       }
-            df <- df |> mutate(s = renumber(s1), s1 = s)
+      df <- df |> mutate(s = renumber(s1), s1 = s)
     }
   }
   # df <- subset(df, !is.na(s), select=c(x,y,s)) |> arrange(s)
@@ -216,9 +216,9 @@ ggplot()+
 dfs <- data.frame(x=crown$x, y=crown$y)
 dfs.rotate <- rotate(dfs$x, dfs$y, 50)
 wmax <- min(c(max(dfs$x)-min(dfs$x),
-      max(dfs$y)-min(dfs$y),
-      max(dfs.rotate$x)-min(dfs.rotate$x),
-      max(dfs.rotate$y)-min(dfs.rotate$y)
+              max(dfs$y)-min(dfs$y),
+              max(dfs.rotate$x)-min(dfs.rotate$x),
+              max(dfs.rotate$y)-min(dfs.rotate$y)
 ))
 
 ggplot()+
@@ -378,69 +378,112 @@ ggplot()+
 
 
 #buffer
-d1 <- cavhull2(df$x,df$y, concavity = 0)
-d1 <- mutate(d1,nx=NA,ny=NA)
-f = 0.5
-f <- f*-1
+d1 <- cavhull(df$x,df$y, concavity = 0)
 
-smax <- max(d1$s, na.rm = TRUE)
-#create inner border to contain convexity
-for(i in 1:smax){#i=5
-  i0 = ifelse(i == 1,smax,i-1)
-  i2 = ifelse(i == smax,1,i+1)
-  x0 <- d1[d1$s %in% i0,]$x
-  y0 <- d1[d1$s %in% i0,]$y
-  x1 <- d1[d1$s %in% i,]$x
-  y1 <- d1[d1$s %in% i,]$y
-  x2 <- d1[d1$s %in% i2,]$x
-  y2 <- d1[d1$s %in% i2,]$y
-  l0 <- ((x1-x0)^2+(y1-y0)^2)^0.5
-  l2 <- ((x1-x2)^2+(y1-y2)^2)^0.5
-  
-  a0 = acos((x1-x0)/l0)
-  a0 = ifelse(y1 - y0 >=0,a0,-1*a0)
-  dfr <- vegnasis::rotate(x=x1, y=y1, a=a0/2/pi*360, cx=x0, cy=y0)
-  xr1 <- dfr$x[1]
-  yr1 <- dfr$y[1]
-  dfr <- vegnasis::rotate(x=x2, y=y2, a=a0/2/pi*360, cx=x0, cy=y0)
-  xr2 <- dfr$x
-  yr2 <- dfr$y
-  a2 <- acos((xr2-xr1)/l2)
-  a2 <- ifelse(yr2 - yr1 >=0,a2,-1*a2)
-  a3 <- pi-(pi-a2)/2
-  yr3 <- f*sin(a3)+yr1
-  xr3 <- f*cos(a3)+xr1
-  dfr2 <- vegnasis::rotate(x=xr3, y=yr3, a=-a0/2/pi*360, cx=x0, cy=y0)
-  nnx <- dfr2$x[1]
-  nny <- dfr2$y[1]
-  d1$nx <- ifelse(d1$s %in% i, nnx,d1$nx)
-  d1$ny <- ifelse(d1$s %in% i, nny,d1$ny)
-}
-#insert new points on inner border
-p0 <- c(0:4)/5
-p1 <- 1-p0
-for(i in 1:nrow(d1)){#i=3
-  i0 = ifelse(i == 1,smax,i-1)
-  x0 <- d1[d1$s %in% (i0),]$nx
-  y0 <- d1[d1$s %in% (i0),]$ny
-  x1 <- d1[d1$s %in% i,]$nx
-  y1 <- d1[d1$s %in% i,]$ny
-  l0 <- ((x1-x0)^2+(y1-y0)^2)^0.5
-  newX0 <- x0*p0+x1*p1
-  newY0 <- y0*p0+y1*p1
-  if(i==1){
-    newY <- newY0
-    newX <- newX0
-  }else{
-    newY <- c(newY,newY0)
-    newX <- c(newX,newX0)
+minXY <- function(x,y){
+  df <- data.frame(x=x,y=y)
+  df22 <- rotate(x=df$x, y=df$y, a=22.5, cx = 0, cy = 0)
+  df45 <- rotate(x=df$x, y=df$y, a=45, cx = 0, cy = 0)
+  df67 <- rotate(x=df$x, y=df$y, a=67.5, cx = 0, cy = 0)
+  x <- pmin(max(df$x) - min(df$x),
+            max(df$y) - min(df$y),
+            max(df22$x) - min(df22$x),
+            max(df22$y) - min(df22$y),
+            max(df45$x) - min(df45$x),
+            max(df45$y) - min(df45$y),
+            max(df67$x) - min(df67$x),
+            max(df67$y) - min(df67$y))
+  return(x)}
+
+
+
+hull.buffer <- function(x, y, s, b){
+  df <- data.frame(x=x,y=y,s=s)
+  df <- mutate(df,nx=NA,ny=NA)
+  #ensure that negative buffer doesn't go pass minimum thickness
+  mxy <- minXY(x=x,y=y)
+  f = ifelse(b < 0, -1*pmin(abs(b),mxy/2),b)
+  smax <- max(df$s, na.rm = TRUE)
+  #create inner border to contain convexity
+  for(i in 1:smax){#i=5
+    i0 = ifelse(i == 1,smax,i-1)
+    i2 = ifelse(i == smax,1,i+1)
+    x0 <- df[df$s %in% i0,]$x
+    y0 <- df[df$s %in% i0,]$y
+    x1 <- df[df$s %in% i,]$x
+    y1 <- df[df$s %in% i,]$y
+    x2 <- df[df$s %in% i2,]$x
+    y2 <- df[df$s %in% i2,]$y
+    l0 <- ((x1-x0)^2+(y1-y0)^2)^0.5
+    l2 <- ((x1-x2)^2+(y1-y2)^2)^0.5
+    
+    a0 = acos((x1-x0)/l0)
+    a0 = ifelse(y1 - y0 >=0,a0,-1*a0)
+    dfr <- vegnasis::rotate(x=x1, y=y1, a=a0/2/pi*360, cx=x0, cy=y0)
+    xr1 <- dfr$x[1]
+    yr1 <- dfr$y[1]
+    dfr <- vegnasis::rotate(x=x2, y=y2, a=a0/2/pi*360, cx=x0, cy=y0)
+    xr2 <- dfr$x
+    yr2 <- dfr$y
+    a2 <- acos((xr2-xr1)/l2)
+    a2 <- ifelse(yr2 - yr1 >=0,a2,-1*a2)
+    a3 <- pi-(pi-a2)/2
+    yr3 <- f*sin(a3)+yr1
+    xr3 <- f*cos(a3)+xr1
+    dfr2 <- vegnasis::rotate(x=xr3, y=yr3, a=-a0/2/pi*360, cx=x0, cy=y0)
+    nnx <- dfr2$x[1]
+    nny <- dfr2$y[1]
+    df$nx <- ifelse(df$s %in% i, nnx,df$nx)
+    df$ny <- ifelse(df$s %in% i, nny,df$ny)
   }
+  #insert new points on inner border
+  
+  for(i in 1:nrow(df)){#i=3
+    i0 = ifelse(i == 1,smax,i-1)
+    x0 <- df[df$s %in% (i0),]$nx
+    y0 <- df[df$s %in% (i0),]$ny
+    x1 <- df[df$s %in% i,]$nx
+    y1 <- df[df$s %in% i,]$ny
+    l0 <- ((x1-x0)^2+(y1-y0)^2)^0.5
+    nseg <- pmin(10,pmax(5,5*floor(3*l0/mxy)))
+    p0 <- c(0:(nseg-1))/nseg
+    p1 <- 1-p0
+    newX0 <- x0*p0+x1*p1
+    newY0 <- y0*p0+y1*p1
+    if(i==1){
+      newY <- newY0
+      newX <- newX0
+    }else{
+      newY <- c(newY,newY0)
+      newX <- c(newX,newX0)
+    }
+  }
+  df1 <- data.frame(x=newX, y=newY)
+  return(df1)
 }
-df1 <- data.frame(x=newX, y=newY)
+
+
+minXY <- function(x,y){
+  df <- data.frame(x=x,y=y)
+  df22 <- rotate(x=df$x, y=df$y, a=22.5, cx = 0, cy = 0)
+  df45 <- rotate(x=df$x, y=df$y, a=45, cx = 0, cy = 0)
+  df67 <- rotate(x=df$x, y=df$y, a=67.5, cx = 0, cy = 0)
+  x <- pmin(max(df$x) - min(df$x),
+            max(df$y) - min(df$y),
+            max(df22$x) - min(df22$x),
+            max(df22$y) - min(df22$y),
+            max(df45$x) - min(df45$x),
+            max(df45$y) - min(df45$y),
+            max(df67$x) - min(df67$x),
+            max(df67$y) - min(df67$y))
+  return(x)}
+
+
+d12 <- hull.buffer(d1$x,d1$y,d1$s, 1.5)
+
 
 ggplot()+
   geom_point(data=d1,aes(x=x,y=y), color='blue', fill='blue')+
-  geom_point(data=df1,aes(x=x,y=y))+
-  geom_point(data=d1,aes(x=nx,y=ny), color='red', fill='red')+
+  geom_point(data=d12,aes(x=x,y=y), color='red', fill='red')+
   coord_fixed()
 
